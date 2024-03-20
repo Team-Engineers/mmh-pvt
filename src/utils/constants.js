194472ae9e39fca0ed061a1f2086ef0ded6a85e4
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2024-03-21 00:34:38 +0530

    api changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API = "https://krteleservices-backend.onrender.com/mmh/api";
+// export const API = "https://krteleservices-backend.onrender.com/mmh/api";
 // export const API = "http://localhost:4000/mmh/api";
-// export const API = "https://api.thekrteleservices.com/mmh/api";
+export const API = "https://api.thekrteleservices.com/mmh/api";
 
 // export const API = "http://3.111.103.148:8800/api";
